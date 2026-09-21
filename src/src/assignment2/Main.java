3b23ff7bdee7e8aebd7352f561eb1e9850643316
asiyaaman <asiya.aman07@gmail.com>
2026-09-21 22:39:21 +0500

    /

--- src/src/assignment2/Main.java
+++ src/src/assignment2/Main.java
@@ -3,10 +3,26 @@ package assignment2;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("PART A:");
         WorkoutFactory runningFactory = new RunningWorkoutFactory();
         runningFactory.logworkout();
 
         WorkoutFactory pushUpFactory = new PushUpWorkoutFactory();
         pushUpFactory.logworkout();
+
+        System.out.println("PART B");
+        System.out.println("Cardio Plan");
+        FitnessFactory cardioFactory = new CardioFitnessFactory();
+        Equipment cardioEquipment = cardioFactory.createEquipment();
+        Nutrition cardioNutrition = cardioFactory.createNutrition();
+        cardioEquipment.use();
+        cardioNutrition.consume();
+
+        System.out.println("Strength Plan");
+        FitnessFactory strengthFactory = new StrengthFitnessFactory();
+        Equipment strengthEquipment = strengthFactory.createEquipment();
+        Nutrition strengthNutrition = strengthFactory.createNutrition();
+        strengthEquipment.use();
+        strengthNutrition.consume();
     }
 }
